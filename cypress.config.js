6c8c902a41665ca5f46468024227b56627972fac
Zeus Viso <zesfpv@gmail.com>
2023-05-05 17:34:27 +0200

    add e2e structure

--- cypress.config.js
+++ cypress.config.js
@@ -6,9 +6,19 @@ const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esb
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       on("file:preprocessor",
       createBundler({
+        reporter: 'cypress-mochawesome-reporter',
+        reporterOptions: {
+          charts: true,
+          reportPageTitle: 'custom-title',
+          embeddedScreenshots: true,
+          inlineAssets: true,
+          saveAllAttempts: false,
+        },
         plugins: [createEsbuildPlugin.default(config)],
+        
       }));
       preprocessor.addCucumberPreprocessorPlugin(on, config);
       return config;

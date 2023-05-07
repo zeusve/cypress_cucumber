30534fe43effcfc88ed75ea0f432b207ab979868
Zeus Viso <zesfpv@gmail.com>
2023-05-07 18:11:04 +0200

    implement allure report and first video report

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,13 @@
 const cucumber = require('cypress-cucumber-preprocessor').default
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = (on, config) => {
+  allureWriter(on, config);
+
+  return config;
+};
 
 module.exports = (on, config) => {
     on('file:preprocessor', cucumber())
     }
+    

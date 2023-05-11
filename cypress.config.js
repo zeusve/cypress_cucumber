3d37ba176e7fe76401bda8177f6e40c9750b6fb5
Zeus Viso <zesfpv@gmail.com>
2023-05-11 23:02:23 +0200

    allure report ok

--- cypress.config.js
+++ cypress.config.js
@@ -2,15 +2,24 @@ const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
-  
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "custom-title",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
 
   e2e: {
     baseUrl: "https://www.saucedemo.com/",
     chromeWebSecurity: false,
     specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       on("file:preprocessor",
       createBundler({
         plugins: [createEsbuildPlugin.default(config)],
@@ -21,7 +30,4 @@ module.exports = defineConfig({
     },
   },
 });
-
-
-
 

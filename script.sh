9093f573827f3b7e3352cd0432d5cb86d56615d8
Zeus Viso <zesfpv@gmail.com>
2023-06-04 18:58:41 +0200

    new Dockerfile & script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-cd home
 apt-get update
 apt-get install -y curl wget git openjdk-11-jdk
 curl -sL https://deb.nodesource.com/setup_14.x | bash -
 apt install nodejs -y
 cd app
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64ls 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 apt -f install -y

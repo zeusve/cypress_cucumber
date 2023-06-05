745b52aa8e7b26aafaa45fd3f5c19e4201f2316a
Zeus Viso <zesfpv@gmail.com>
2023-06-05 10:10:40 +0200

    Add comments script.sh

--- script.sh
+++ script.sh
@@ -1,20 +1,39 @@
 #!/bin/sh
 
+# Update ubuntu
 apt-get update
+
+# Install curl, web get, git and java
 apt-get install -y curl wget git openjdk-11-jdk
+
+# Download nodejs, install nodejs and node package manager
 curl -sL https://deb.nodesource.com/setup_14.x | bash -
 apt install nodejs -y
-cd app
+apt install npm -y
+
+# Config Java path
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64ls 
+
+# Download chrome and install
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
-apt -f install -y
-apt install npm -y
-npm install
-npm install cypress
-apt-get update
-apt install xvfb -y && apt install libgtk-3-0 -y && apt install libgbm1 -y && apt-add-repository ppa:qameta/allure
-apt-get update
+
+# Change directory to 'app'
+cd app
+
+# Install necessary libraries for project execution
+apt install xvfb -y && apt install libgtk-3-0 -y && apt install libgbm1 -y 
+
+# Add tthis line at the end of the file /etc/apt/sources.list, install allure and allure comandline
 echo "deb http://ppa.launchpad.net/qameta/allure/ubuntu trusty main" | tee -a /etc/apt/sources.list
 apt install allure -y
 npm install -g allure-commandline
+
+# Install cypress
+npm install cypress
+
+# Install project dependencies
+npm install 
+
+# Fix potential broken dependencies
+apt -f install -y

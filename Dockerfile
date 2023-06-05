62655b4232d5eead5f577986f4dad16665c7e3a9
Zeus Viso <zesfpv@gmail.com>
2023-06-05 16:46:29 +0200

    new Dockerfile & script.sh

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,9 @@
 # Utilizar una imagen base de Ubuntu
 FROM ubuntu:latest
 
-# Instalaciones
-RUN apt update -y && apt install -y git
-
 # Configurar directorio de trabajo
 WORKDIR /app
 
-# Clonar el repositorio Git dentro del contenedor
-RUN git clone https://github.com/zeusve/cypress_cucumber.git /app
-
-# Permisos ejecución 
-RUN chmod +x /app/script.sh
 
 
 
--- script.sh
+++ script.sh
@@ -4,12 +4,11 @@
 apt-get update
 apt-get upgrade -y
 
+# Update and Install git
+apt update -y && apt install -y git
+
 # Install curl, wget, git, and Java
 apt-get install -y curl wget git openjdk-11-jdk
-
-# Download Node.js, install Node.js and npm
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
-apt-get install -y nodejs
 
 # Configure Java path
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
@@ -18,21 +17,33 @@ export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 
+# Clone repository
+git clone https://github.com/zeusve/cypress_cucumber.git 
+
+# Change to the project directory
+cd cypress_cucumber
+
+# Changue permissions of the script.sh
+chmod +x script.sh
 
 # Install necessary libraries for project execution
 apt-get install -y xvfb libgtk-3-0 libgbm1
+
+# Fix potential broken dependencies
+apt-get -f install -y
+
+# Download Node.js, install Node.js and npm
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash 
+apt-get install -y nodejs
+
+# Install project dependencies
+npm install
+
+# Install Cypress
+npm install cypress
 
 # Add this line at the end of the file /etc/apt/sources.list, install Allure and Allure commandline
 echo "deb http://ppa.launchpad.net/qameta/allure/ubuntu trusty main" | tee -a /etc/apt/sources.list
 apt-get update
 apt-get install -y allure
 npm install -g allure-commandline
-
-# Fix potential broken dependencies
-apt-get -f install -y
-
-# Install Cypress
-npm install cypress
-
-# Install project dependencies
-npm install
